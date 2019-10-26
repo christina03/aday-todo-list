8d77d7cfa62656bb134ad7a356bd445ff2d0e093
christina03 <m18890368211@163.com>
2019-10-26 21:58:22 +0800

    style：修改页面布局

--- src/components/CreateItem.vue
+++ src/components/CreateItem.vue
@@ -1,17 +1,23 @@
 <template>
     <el-form ref="form" :model="form" label-width="100px">
-      <el-form-item label="时间">
-        <el-col :span="11">
-          <el-time-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-time-picker>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="时间段">
+            <el-col :span="11">
+              <el-time-picker type="date" placeholder="选择时间" v-model="form.date1" style="width: 100%;"></el-time-picker>
+            </el-col>
+            <el-col class="line" :span="2">-</el-col>
+            <el-col :span="11">
+              <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+            </el-col>
+          </el-form-item>
         </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+        <el-col :span="12">
+          <el-form-item label="待办项名称">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
         </el-col>
-      </el-form-item>
-      <el-form-item label="待办项名称">
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
+      </el-row>
       <el-form-item label="详细描述">
         <el-input type="textarea" v-model="form.desc"></el-input>
       </el-form-item>
